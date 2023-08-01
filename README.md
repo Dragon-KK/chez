31e541e34728ec321789b0c0933c140ef8dd3392
nandhagk <nandhagopi@gmail.com>
2023-08-01 22:48:57 +0530

    Clean Slate

--- README.md
+++
@@ -1,2 +0,0 @@
-# chez
-ez change
--- tasks/test.bat
+++
@@ -1,4 +0,0 @@
-
-g++ main.cpp -o main -lmingw32 -lglew32 -lglu32 -lopengl32 -lglfw3 -lkernel32 -lgdi32 -luser32 -lws2_32 -Wl,-u,___mingw_vsnprintf -Wl,--defsym,___ms_vsnprintf=___mingw_vsnprintf
-
-main
